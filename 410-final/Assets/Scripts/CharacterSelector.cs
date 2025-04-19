47a3b3167d55b08080c3fe56f139382af35c0b2e
Kylie Griffiths <kcg3@icloud.com>
2025-04-19 13:07:42 -0700

    working on initial scenes

--- 410-final/Assets/Scripts/CharacterSelector.cs
+++ 410-final/Assets/Scripts/CharacterSelector.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelector : MonoBehaviour
 {
+
+        public GameObject[] playerChoices;
+        private int selectedCharacter;
+        public string mainScene = "CharacterCustomization"; //TO BE CHANGED
+        private string selectedCharacterName = "SelectedCharacter";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        HideAllCharacters();
+        selectedCharacter = PlayerPrefs.GetInt(selectedCharacterName, 0);
+        playerChoices[selectedCharacter].SetActive(true);
+
+    }
+    public void HideAllCharacters() {
+        foreach (GameObject g in playerChoices) {
+            g.SetActive(false);
+        }
+    }
+    public void NextCharacter() {
+        playerChoices[selectedCharacter].SetActive(false);
+        selectedCharacter++;
+        if (selectedCharacter >= playerChoices.Length) {
+            selectedCharacter = 0;
+        }
+        playerChoices[selectedCharacter].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PreviousCharacter() {
+        playerChoices[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0) {
+            selectedCharacter = playerChoices.Length - 1;
+        }
+        playerChoices[selectedCharacter].SetActive(true);
+    }
+
+    public void StartGame() {
+        PlayerPrefs.SetInt(selectedCharacterName, selectedCharacter);
+        SceneManager.LoadScene(mainScene);
     }
 }

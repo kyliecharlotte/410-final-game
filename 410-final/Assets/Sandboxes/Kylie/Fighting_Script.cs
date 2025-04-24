903decc7b4c0855cf182853e8eab91dd100399cb
Kylie Griffiths <kcg3@icloud.com>
2025-04-24 09:56:03 -0700

    put stuff in kylie folder for combat

---
+++ 410-final/Assets/Sandboxes/Kylie/Fighting_Script.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Fighting_Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+public CharacterController controller;
+    public GameObject mainScript;
+    public Transform playerBody;
+    
+    public Vector3 inputDirection;
+    public GameObject player;
+    private float playerSpeed = 7.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = player.GetComponent<CharacterController>();
+        playerBody = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        inputDirection = new Vector3(x,0,z);
+        Vector3 worldInputDirection = playerBody.TransformDirection(inputDirection);
+        
+        controller.Move(worldInputDirection * Time.deltaTime * playerSpeed);
+
+       /* if (Input.GetAxisRaw("Horizontal") > 0) {
+            RotatePlayer(1);
+        } else if (Input.GetAxisRaw("Horizontal") < 0) {
+            RotatePlayer(-1);
+        }*/
+
+        if (inputDirection.magnitude > 0.01f) {
+            inputDirection.Normalize();
+            if (z >= 0) {
+                if (x > 0) {
+                //playerBody.rotation = Quaternion.Lerp(playerBody.rotation, Quaternion.LookRotation(inputDirection), 10f * Time.deltaTime);
+                    playerBody.Rotate(Vector3.up * 200f * Time.deltaTime);
+                } else if (x < 0) {
+                    playerBody.Rotate(-1 * Vector3.up * 200f * Time.deltaTime);
+                }
+            }
+            //playerBody.rotation = Quaternion.Lerp(playerBody.rotation, Quaternion.LookRotation(inputDirection), 10f * Time.deltaTime);
+        }
+    }  
+}

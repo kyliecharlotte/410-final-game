787d056a6b6e53898fcb5e5f31d6ed07561b80b8
Kylie Griffiths <kcg3@icloud.com>
2025-04-23 23:05:37 -0700

    small door tweaks

--- 410-final/Assets/Sandboxes/Kylie/Level_Start_Doors.cs
+++ 410-final/Assets/Sandboxes/Kylie/Level_Start_Doors.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Runtime.ConstrainedExecution;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class Level_Start_Doors : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private GameObject player;
+
+    public bool opened;
     public GameObject doors;
     public GameObject mainScript;
 
@@ -13,6 +16,7 @@ public class Level_Start_Doors : MonoBehaviour
     {
         Main_PlayerSelect script = mainScript.GetComponent<Main_PlayerSelect>();
         player = script.returnPlayer();
+        opened = false;
     }
 
     // Update is called once per frame
@@ -27,23 +31,42 @@ public class Level_Start_Doors : MonoBehaviour
     void CheckDoor() {
 
         Debug.Log((doors.transform.position - player.transform.position).magnitude);
-        if ((doors.transform.position - player.transform.position).magnitude < 10f) {
+        if (((doors.transform.position - player.transform.position).magnitude < 10f) && (opened == false)) {
             GameObject door_one = doors.gameObject.transform.GetChild(0).gameObject;
             GameObject door_two = doors.gameObject.transform.GetChild(1).gameObject;
-            door_one.transform.position = new Vector3(0,0,0);
-            door_two.transform.position = new Vector3(0,0,0);
+            AnimateOpeningDoor(door_one, door_two);
+            
 
         }
     }
 
-    /*void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == doors) {
-            GameObject door_one = other.gameObject.transform.GetChild(0).gameObject;
-            GameObject door_two = other.gameObject.transform.GetChild(1).gameObject;
-            door_one.transform.position = new Vector3(0,0,0);
+    private IEnumerator Rotate90(GameObject door_one, GameObject door_two) {
+        float timeElapsed = 0;
+        Quaternion one_start = door_one.transform.rotation;
+        Quaternion one_target = door_one.transform.rotation * Quaternion.Euler(0,270,0);
+        
+        Quaternion two_start = door_two.transform.rotation;
+        Quaternion two_target = door_two.transform.rotation * Quaternion.Euler(0,-270,0);
+        
+        while (timeElapsed < 0.9f) {
+            door_one.transform.rotation = Quaternion.Lerp(one_start, one_target, timeElapsed);
+            door_two.transform.rotation = Quaternion.Lerp(two_start, two_target, timeElapsed);
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
-    }*/
-    
+        door_one.transform.rotation = one_target;
+        door_two.transform.rotation = two_target;
+        door_one.transform.position = new Vector3(door_one.transform.position.x - 0.5f, door_one.transform.position.y, door_one.transform.position.z);
+        door_two.transform.position = new Vector3(door_two.transform.position.x+0.1f, door_two.transform.position.y, door_two.transform.position.z + 0.7f);
+
+    }
+
+    void AnimateOpeningDoor(GameObject door_one, GameObject door_two) {
+            opened = true;
+            door_one.transform.position = new Vector3(door_one.transform.position.x - 1.8f, door_one.transform.position.y, door_one.transform.position.z + 0.9f);
+            door_two.transform.position = new Vector3(door_two.transform.position.x + 0.3f, door_two.transform.position.y, door_two.transform.position.z + 1.5f);
+            doors.GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(Rotate90(door_one, door_two));
+    }
 
 }

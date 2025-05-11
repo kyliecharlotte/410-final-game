ddaa929b9ba612396187baeaf4fba2b35d30c7fc
Lily Logan <lily.logan1018@gmail.com>
2025-05-10 19:47:40 -0700

    decreased assets

---
+++ 410-final/Assets/Sandboxes/Lily/scripts/BarScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class BarScript : MonoBehavior
+{
+    // [SerializedField]
+    private float fillAmount;
+    void Start() 
+    {
+
+    }
+
+    void Update() 
+    {
+        HandleBar();
+    }
+
+    private void HandleBar() 
+    {
+
+    }
+}

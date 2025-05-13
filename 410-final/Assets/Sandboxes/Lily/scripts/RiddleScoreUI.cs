c41ce13ca876aad78d34ad98311914fa4a3ecc1d
Lily Logan <lily.logan1018@gmail.com>
2025-05-13 16:32:21 -0700

    ui - riddle and weapon

--- 410-final/Assets/Sandboxes/Lily/scripts/RiddleScoreUI.cs
+++ 410-final/Assets/Sandboxes/Lily/scripts/RiddleScoreUI.cs
@@ -1,9 +1,36 @@
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class RiddleScoreUI : MonoBehaviour
+// {
+//     [SerializeField] private Text scoreText; // Reference to the UI Text element
+
+//     void Start()
+//     {
+//         if (scoreText == null)
+//         {
+//             Debug.LogError("Score Text is not assigned! Drag the UI text object into the field.");
+//         }
+//         UpdateScoreUI();
+//     }
+
+//     public void UpdateScoreUI()
+//     {
+//         if (RiddleManager.instance != null)
+//         {
+//             scoreText.text = "Riddle Pieces: " + RiddleManager.instance.ReturnScore() + "/3";
+//         }
+//     }
+// }
+
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RiddleScoreUI : MonoBehaviour
 {
-    [SerializeField] private Text scoreText; // Reference to the UI Text element
+    [SerializeField] private Text scoreText; // Reference to the UI text
+    [SerializeField] private Image[] riddleImages; // Array of images
 
     void Start()
     {
@@ -11,14 +38,42 @@ public class RiddleScoreUI : MonoBehaviour
         {
             Debug.LogError("Score Text is not assigned! Drag the UI text object into the field.");
         }
-        UpdateScoreUI();
+
+        if (riddleImages.Length != 3)
+        {
+            Debug.LogError("Assign exactly 3 images for the riddle UI!");
+        }
+
+        UpdateScoreUI(); // Initialize UI
     }
 
     public void UpdateScoreUI()
     {
         if (RiddleManager.instance != null)
         {
-            scoreText.text = "Riddle Pieces: " + RiddleManager.instance.ReturnScore() + "/3";
+            int score = RiddleManager.instance.ReturnScore();
+            scoreText.text = "Riddle Pieces: " + score + "/3";
+
+            UpdateRiddleImages(score);
+        }
+    }
+
+    private void UpdateRiddleImages(int score)
+    {
+        for (int i = 0; i < riddleImages.Length; i++)
+        {
+            Color imgColor = riddleImages[i].color;
+
+            if (i < score)
+            {
+                imgColor.a = 1f; // Fully visible
+            }
+            else
+            {
+                imgColor.a = 0.3f; // Semi-transparent
+            }
+
+            riddleImages[i].color = imgColor;
         }
     }
 }

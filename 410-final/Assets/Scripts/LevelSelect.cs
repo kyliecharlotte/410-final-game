65f631d95a8ad34067d0c88ae972249d0eca940d
Nithi <147197457+nithideiv@users.noreply.github.com>
2025-06-02 19:26:41 -0700

    placeholder for the level button sound

--- 410-final/Assets/Scripts/LevelSelect.cs
+++ 410-final/Assets/Scripts/LevelSelect.cs
@@ -8,6 +8,8 @@ public class LevelSelect : MonoBehaviour
     int selectedCharacter;
     private string selectedCharacterName = "SelectedCharacter";
 
+    public AudioSource clickSoundSource;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,18 +17,30 @@ public class LevelSelect : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         selectedCharacter = PlayerPrefs.GetInt(selectedCharacterName, 0);
+
+    /*   
+        if (clickSoundSource == null)
+        {
+            Debug.LogWarning("Click sound source not assigned in the Inspector!");
+        }
+
+        AddClickListenersToAllButtons();
+
+    */
     }
 
     // Update is called once per frame
 
     public void Level_One()
     {
+        //PlayClickSound();
         PlayerPrefs.SetInt(selectedCharacterName, selectedCharacter);
         SceneManager.LoadScene("castle");
     }
 
     public void Level_Two()
     {
+        //PlayClickSound();
         if (PlayerPrefs.HasKey("castle"))
         {
             PlayerPrefs.SetInt(selectedCharacterName, selectedCharacter);
@@ -36,12 +50,32 @@ public class LevelSelect : MonoBehaviour
 
     public void Level_Three()
     {
+        //PlayClickSound();
         if (PlayerPrefs.HasKey("castle 2"))
         {
             PlayerPrefs.SetInt(selectedCharacterName, selectedCharacter);
             SceneManager.LoadScene("castle 3");
         }
     }
+
+    /*
+    private void PlayClickSound()
+    {
+        if (clickSoundSource != null)
+        {
+            clickSoundSource.Play();
+        }
+    }
+
+    private void AddClickListenersToAllButtons()
+    {
+        var buttons = FindObjectsOfType<Button>();
+        foreach (var button in buttons)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
+    }
+    */
 
     void Update()
     {
